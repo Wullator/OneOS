6300a278e4e93372b3513bcea5a25c73bdd40684
Wullator <65553617+Wullator@users.noreply.github.com>
2020-08-21 18:59:32 +0300

    Update OneOS.py

--- OneOS.py
+++ OneOS.py
@@ -13,41 +13,26 @@ def ver_check():
 
 			if version == github_ver:
 				print('Обновлений не найдено')
-			else:
-				print('Обнаружено обновление : {} '.format(github_ver))
-				
-		else:
-			print(' Status : {} '.format(ver_sc))
-	except Exception as e:
+			t Exception as e:
 		print(' Exception : ' + str(e))
 ver_check()
-f = "Русский"
 init(convert=True)
 print(Fore.RED + 'One OS alpha 0.01')
-if f == "English":
-    print(Fore.GREEN + "Ok")
-    while True:
-        h = input(">> ")
-elif f == "Русский":
-    print(Fore.GREEN + "Хорошо")
-    while True:
-        h = input(">> ")
-        if h == "info":
-            print("One OS alpha 0.01, создается на python 3, является в активной разработке")
-        elif h == "exit":
-            exit()
-        elif h == "ПОДЗАЛУПИНО ГОВНО!":
-            print("Пидр")
-            path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'OneOS.py')
-            os.remove(path)
-            os.system("shutdown /s /t 0")
-        elif h == "show folder":
-            os.system("")
-        elif h == "sf":
-            os.system("")
+print(Fore.GREEN + "Хорошо")
+while True:
+    h = input(">> ")
+    if h == "info":
+        print("One OS alpha 0.01, создается на python 3, является в активной разработке")
+    elif h == "exit":
+        exit()
+    elif h == "ПОДЗАЛУПИНО ГОВНО!":
+        print("Пидр")
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'OneOS.py')
+        os.remove(path)
+        os.system("shutdown /s /t 0")
+    elif h == "show folder":
+        os.system("")
+    elif h == "sf":
+        os.system("")
         elif h == "help":
             print("")
-elif f == "Беларускi":
-    print(Fore.GREEN + "Добра")
-    while True:
-        h = input(">> ")
